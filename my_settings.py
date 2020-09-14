c888b46720e8191a6a499f17b6a64c4cf4d135d3
Soojeong Kim <ksooj.kim@gmail.com>
2020-09-14 20:38:54 +0900

    초기세팅

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'allstar',
+        'USER': 'root',
+        'PASSWORD': 'wecode',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+SECRET_KEY = '&z+xf3*q5sjsf6^=-2w)-&i*x#9*n49yl%lc-33#gzycv=592_'
+ALGORITHM = 'HS256'
