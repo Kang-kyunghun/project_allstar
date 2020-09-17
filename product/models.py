992894e14b36df82fc14fc88d3f9a04cb3854fb0
Kang-Kyunghun <korea271150@gmail.com>
2020-09-17 14:48:28 +0900

    Edit & Migration-migrate: product_models.py

--- product/models.py
+++ product/models.py
@@ -1,7 +1,7 @@
 from django.db              import models
 
 class Category(models.Model):
-    category                = models.CharField(max_lenght = 50)
+    category                = models.CharField(max_length = 50)
   
     def __str__(self):
         return self.category 
@@ -10,7 +10,7 @@ class Category(models.Model):
         db_table            = 'categories'
 
 class SubCategory(models.Model):
-    sub_category            = models.CharField(max_lenght = 50)
+    sub_category            = models.CharField(max_length = 50)
   
     def __str__(self):
         return self.sub_category 
@@ -19,8 +19,8 @@ class SubCategory(models.Model):
         db_table            = 'sub_categories'
 
 class Series(models.Model):
-    name                    = models.CharField(max_lenght = 100)
-    detail_description      = models.TesxField()
+    name                    = models.CharField(max_length = 100)
+    detail_description      = models.TextField()
     feature                 = models.URLField()
     additional_image        = models.URLField()
     image                   = models.URLField()
@@ -32,7 +32,7 @@ class Series(models.Model):
         db_table            = 'serieses'
 
 class Sex(models.Model):
-    sex                     = models.CharField(max_lenght = 50)
+    sex                     = models.CharField(max_length = 50)
 
     def __str__(self):
         return self.sex 
@@ -41,7 +41,7 @@ class Sex(models.Model):
         db_table            = 'sexes'
 
 class FilteringColor(models.Model):
-    filtering_color         = models.CharField(max_lenght = 100)
+    filtering_color         = models.CharField(max_length = 100)
 
     def __str__(self):
         return self.filtering_color 
@@ -50,8 +50,8 @@ class FilteringColor(models.Model):
         db_table            = 'filtering_colors'
 
 class Color(models.Model):
-    color                   = models.CharField(max_lenght = 100)
-    filtering_color         = models.ForeingKey(FilteringColor, on_delete = models.CASCADE) 
+    color                   = models.CharField(max_length = 100)
+    filtering_color         = models.ForeignKey(FilteringColor, on_delete = models.CASCADE) 
 
     def __str__(self):
         return self.color 
@@ -60,7 +60,7 @@ class Color(models.Model):
         db_table            = 'colors'
 
 class Promotion(models.Model):
-    promotion               = models.CharField(max_lenght = 50, null = True)
+    promotion               = models.CharField(max_length = 50, null = True)
     
     def __str__(self):
         return self.promotion 
@@ -69,7 +69,7 @@ class Promotion(models.Model):
         db_table            = 'promotions'
 
 class Silouette(models.Model):
-    silouette               = models.CharField(max_lenght = 50)
+    silouette               = models.CharField(max_length = 50)
     
     def __str__(self):
         return self.silouette 
@@ -87,20 +87,20 @@ class Size(models.Model):
         db_table            = 'sizes'
 
 class Product(models.Model):
-    serial_number           = models.CharField(max_lenght = 50)
-    category                = models.ForeingKey(Category, on_delete = models.CASCADE)
-    sub_category            = models.ForeingKey(SubCategory, on_delete = models.CASCADE)
-    serise                  = models.ForeingKey(Series, on_delete = models.CASCADE)
+    serial_number           = models.CharField(max_length = 50)
+    category                = models.ForeignKey(Category, on_delete = models.CASCADE)
+    sub_category            = models.ForeignKey(SubCategory, on_delete = models.CASCADE)
+    serise                  = models.ForeignKey(Series, on_delete = models.CASCADE)
     main_image              = models.URLField()
     hover_image             = models.URLField()
     price                   = models.IntegerField(default = 0)
     discount_rate           = models.IntegerField(default = 0)
-    sex                     = models.ForeingKey(Sex, on_delete = models.CASCADE)
-    simple_description      = models.CharField(max_lenght = 200)
-    color                   = models.ForeingKey(Color, on_delete = models.CASCADE)
+    sex                     = models.ForeignKey(Sex, on_delete = models.CASCADE)
+    simple_description      = models.CharField(max_length = 200)
+    color                   = models.ForeignKey(Color, on_delete = models.CASCADE)
     size                    = models.ManyToManyField(Size) 
-    promotion               = models.ForeingKey(Promotion, on_delete = models.CASCADE)
-    silouette               = models.ForeingKey(Silouette, on_delete = models.CASCADE)
+    promotion               = models.ForeignKey(Promotion, on_delete = models.CASCADE)
+    silouette               = models.ForeignKey(Silouette, on_delete = models.CASCADE)
     score                   = models.DecimalField(max_digits = 5, decimal_places = 2)
     created_at              = models.DateTimeField(auto_now_add = True)
     updated_at              = models.DateTimeField(auto_now = True) 
@@ -112,24 +112,24 @@ class Product(models.Model):
         db_table            = 'products'
 
 class Medium(models.Model):
-    product                 = models.ForeingKey(Product, on_delete = models.CASCADE)
+    product                 = models.ForeignKey(Product, on_delete = models.CASCADE)
     medium_url              = models.URLField()
-    medium_type             = models.CharField(max_lenght = 100)
+    medium_type             = models.CharField(max_length = 100)
     
     class Meta:
         db_table            = 'media'
 
 class ProductInformation(models.Model):
-    product                 = models.ForeingKey(Product, on_delete = models.CASCADE)
-    material                = models.CharField(max_lenght = 100) 
-    minimum_size            = models.ForeingKey(Size, on_delete = models.CASCADE)
-    maximum_size            = models.ForeingKey(Size, on_delete = models.CASCADE)
-    manufacture             = models.CharField(max_lenght = 100)
-    country                 = models.CharField(max_lenght = 100)
-    caution                 = models.CharField(max_lenght = 200)
-    quality_assureance      = models.CharField(max_lenght = 100)
-    as_center               = models.CharField(max_lenght = 100)
-    manufacture_date        = models.CharField(max_lenght = 100)
+    product                 = models.ForeignKey(Product, on_delete = models.CASCADE)
+    material                = models.CharField(max_length = 100) 
+    minimum_size            = models.IntegerField(default = 0)
+    maximum_size            = models.IntegerField(default = 0)
+    manufacture             = models.CharField(max_length = 100)
+    country                 = models.CharField(max_length = 100)
+    caution                 = models.CharField(max_length = 200)
+    quality_assureance      = models.CharField(max_length = 100)
+    as_center               = models.CharField(max_length = 100)
+    manufacture_date        = models.CharField(max_length = 100)
     
     class Meta:
         db_table            = 'product_informations'
