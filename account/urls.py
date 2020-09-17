51896cfae3a019a49191af02be92e76e287e3080
Soojeong Kim <ksooj.kim@gmail.com>
2020-09-17 14:19:00 +0900

    [12기 김수정] - account 앱 생성 및 models.py 작성 - account 모델 작성 및 migrate 완료 - settings.py 에 앱 추가 - models.py 내 wishlist class는 product 앱 필요하여 주석처리 함

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import 
+
+urlpatterns = [
+        #path('/signup', SignUpView.as_view()), - view 작성 이후 주석처리 제거
+        #path('/signin', SignInView.as_view()), - view 작성 이후 주석처리 제거
+        #path('/wishlist', WishlistView.as_view()), - product 앱 merging 이후 주석처리 제거
+]
