f2e6f03ec0541e602049b5dfe76e84ce87e4f299
Soojeong Kim <ksooj.kim@gmail.com>
2020-09-21 20:55:58 +0900

    ADD_[product] 상세페이지 구현

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,5 @@ from django.urls        import path
 from .views             import DetailView
 
 urlpatterns = [
-    path('/<str:serialNumber>', DetailView.as_view()),
+    path('/<int:id>', DetailView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,27 +1,44 @@
 import json
+
 from django.http        import JsonResponse
 from django.views       import View
-from .models            import FilteringColor, Product, SubCategory, Series, Sex, Color, ProductSize, Size, Medium, ProductInformation 
+
+from account.models     import Wishlist
+from .models            import (FilteringColor, 
+                                Product, 
+                                SubCategory, 
+                                Series, 
+                                Sex, 
+                                Color, 
+                                ProductSize, 
+                                Size, 
+                                Medium, 
+                                ProductInformation, 
+                                Promotion)
 
 class DetailView(View):
-    def get(self, request, serialNumber):
-        product = Product.objects.get(serial_number=serialNumber)
+    def get(self, request, id):
+        
+        product = Product.objects.get(id=id)
         
         information_dict = {}
 
         information_dict['sub_category']        = product.sub_category.name
         information_dict['series_name']         = product.series.name
-        information_dict['price']               = product.price
+        information_dict['price']               = int(product.price)
         information_dict['discount_rate']       = product.discount_rate
-        information_dict['promotion']           = product.promotion.name
         information_dict['sex']                 = product.sex.name
         information_dict['simple_description']  = product.simple_description
         information_dict['color']               = product.color.name
-        information_dict['serial_number']       = serialNumber
+        information_dict['serial_number']       = product.serial_number
         information_dict['main_image']          = product.main_image
                 
-        sub_images = [i.medium_url for i in Medium.objects.filter(product=product.id)]
+        sub_images = [{
+            'medium_url'    : i.medium_url,
+            'medium_type'   : i.medium_type} 
+            for i in Medium.objects.filter(product=product.id)]
         information_dict['sub_images']          = sub_images
+
 
         information_dict['detail_description']  = product.series.detail_description
         information_dict['features']            = product.series.feature
@@ -37,14 +54,50 @@ class DetailView(View):
         information_dict['as_center']           = product_information.as_center
         information_dict['manufacture_date']    = product_information.manufacture_date
 
-        size_list = []
+        try: 
+            if product.promotion.name:
+                information_dict['promotion']   = product.promotion.name
+        except AttributeError:
+            information_dict['promotion']   = None
+
+        series_products = Product.objects.filter(series=product.series)
+        related_products = [
+            {'serial_number'    : series_product.serial_number,
+            'main_image'        : Medium.objects.filter(product_id=series_product.id)[3].medium_url}
+            for series_product in series_products]
+        information_dict['related_products'] = related_products
+
         size_ids = ProductSize.objects.filter(product_id = product.id)
-        for size_id in size_ids:
-            size = size_id.size.name
-            size_list.append(size)
-        
-        size_list.sort()
-        information_dict['size_range']        = str(size_list[0])+' - '+str(size_list[-1])
+        size_list = sorted([size_id.size.name for size_id in size_ids])
         information_dict['size_list']         = size_list
-        
+        information_dict['size_range']        = str(size_list[0])+' - '+str(size_list[-1])
+
+        recommendations = Product.objects.filter(sex=product.sex, price=product.price)[0:10]
+        try:
+            recommended_products = [
+                {'main_image':recommendation.main_image,
+                'hover_image':recommendation.hover_image,
+                'serial_number':recommendation.serial_number,
+                'promotion':recommendation.promotion.name} for recommendation in recommendations
+                ]
+        except AttributeError:
+            recommended_products = [
+                {'main_image':recommendation.main_image,
+                'hover_image':recommendation.hover_image,
+                'serial_number':recommendation.serial_number,
+                'promotion':None}  for recommendation in recommendations
+                ]
+
+        information_dict['recommended_product'] = recommended_products
+
+        is_wishlist = False
+        try :
+            if Wishlist.objects.get(product_id=product.id, account_id=request.account_id):
+                is_wishlist = True
+                information_dict['is_wishlist'] = is_wishlist
+        except Wishlist.DoesNotExist :
+            pass
+        except AttributeError : 
+            pass
+      
         return JsonResponse({'product_information': [information_dict]}, status=200)
