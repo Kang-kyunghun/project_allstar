f0a8ff45db7a59909c0ace8569a99eb3507ecbcb
Kang-Kyunghun <korea271150@gmail.com>
2020-09-22 11:29:49 +0900

    Edit: list comprehension

--- product/views.py
+++ product/views.py
@@ -19,12 +19,8 @@ from .models        import (Category,
     
 class ListView(View):   
     def get(self, request, sub_category_id):
-        
         try:
             sub_category             = SubCategory.objects.get(id =  sub_category_id)
-        except:
-            return JsonResponse({'message':'INVALID_VALUE'}, status= 401)
-        else:
             if sub_category.name == '전체보기':
                 products             = Product.objects.all()
             elif sub_category.name in ['베스트셀러', 'MEMBERS ONLY', 'SALE'] :          
@@ -32,28 +28,23 @@ class ListView(View):
             else:
                 products = sub_category.product_set.all()
             
-            products_list             =[]
-            for product in products:
-                color_products_list   = []
-                series_id             = product.series.id
-                color_products        = Product.objects.filter(series_id = series_id)
-                
-                for color_product in color_products:
-                    color_products_list.append({
-                            'id'          : color_product.id,
-                            'image_url'   : [color_product.main_image, color_product.hover_image],
-                            'color'       : color_product.color.filtering_color.name
-                    }) 
-                
-                products_list.append({
-                    "main_image":{
-                            'id'          : product.id,
-                            'price'       : int(product.price),
-                            'image_url'   : [product.main_image, product.hover_image],
-                            'series_name' : product.series.name
-                        },
-                    "color_image" : color_products_list
-                })
-        
-            return JsonResponse({'products': products_list}, status=200)
+        except:
+            return JsonResponse({'message':'INVALID_VALUE'}, status= 401)
+        else:
+            products_list=[{
+                "main_image":{
+                        'id'          : product.id,
+                        'price'       : int(product.price),
+                        'image_url'   : [product.main_image, product.hover_image],
+                        'series_name' : product.series.name
+                    },
+
+                "color_image" : [{
+                        'id'          : color_product.id,
+                        'image_url'   : [color_product.main_image, color_product.hover_image],
+                        'color'       : color_product.color.filtering_color.name
+            }  for color_product in Product.objects.filter(series_id = product.series.id)]
+            } for product in products]
+    
+        return JsonResponse({'products': products_list}, status=200)
 
