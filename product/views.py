50b81c039ecab32679aba0339cf73ce1982ff9ac
Kang-Kyunghun <korea271150@gmail.com>
2020-09-24 09:41:21 +0900

    Edit: sub_category_id로 전체보기 안 되었던 문제 해결, Add: filter 기능 추가(color, size, silouette)

--- product/views.py
+++ product/views.py
@@ -29,18 +29,29 @@ class MainView(View):
 class ListView(View):   
     def get(self, request):
         try:
-            sub_category_id    = request.GET.get('sub_category_id', None) 
-            if sub_category_id == None:
-                products       = Product.objects.all()
-            else:
-                sub_category   = SubCategory.objects.prefetch_related('product_set__series', 'product_set__color__filtering_color', 'product_set__promotion').get(id =  sub_category_id)
+            sub_category_id  = request.GET.get('sub_category_id') 
+            color_filter     = request.GET.getlist('color')
+            silouette_filter = request.GET.getlist('silouette') 
+            size_filter      =  request.GET.getlist('size') 
+
+            sub_category     = SubCategory.objects.prefetch_related(
+                                        'product_set__series', 
+                                        'product_set__color__filtering_color', 
+                                        'product_set__promotion').get(id = sub_category_id)
                 
-                if Promotion.objects.filter(name = sub_category.name) :          
-                    products   = Promotion.objects.get(name = sub_category.name).product_set.all()
-                else:
-                    products   = sub_category.product_set.all()      
+            if Promotion.objects.filter(name = sub_category.name).exists():          
+                products     = Promotion.objects.get(name = sub_category.name).product_set.all()
+            elif sub_category.product_set.exists():
+                products     = sub_category.product_set.all()
+            else:
+                products     = Product.objects.all()   
+            
+            products         = products.filter(color__filtering_color__name__in = color_filter) if color_filter else products
+            products         = products.filter(silouette__name__in = silouette_filter) if silouette_filter else products
+            products         = products.filter(size__name__in = size_filter) if size_filter else products
+        
         except:
-            return JsonResponse({'message':'INVALID_VALUE'}, status= 401)
+            return JsonResponse({'message':'INVALID_VALU'}, status= 401)
         else:
             products_list=[{
                 "main_image":{
