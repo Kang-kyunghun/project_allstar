15ba77364516ad243ef4cc473e35ea4ede3c5e26
Soojeong Kim <ksooj.kim@gmail.com>
2020-09-24 21:45:23 +0900

    수정요청사항반영

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,5 @@
 from django.urls        import path
-<<<<<<< HEAD
-from .views             import ProductView, ListView, MainView
-=======
 from .views             import ProductView, MainView, ListView
->>>>>>> master
 
 urlpatterns = [
     path('/<int:id>', ProductView.as_view()),
